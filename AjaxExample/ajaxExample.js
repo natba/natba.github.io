3b39fe7aadd3585147f1dcf0f100cf7aaf0d97bc
natba <natba@hotmail.com>
2018-11-26 14:03:55 -0800

    Ajax Example fixes / changes

---
+++ AjaxExample/ajaxExample.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $("#section1").load("demo_test.txt", function(responseTxt, statusTxt, xhr){
+      if(statusTxt == "success")
+        alert("External content loaded successfully!");
+
+        if(statusTxt == "error")
+         alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
+  });
+
+  $("#getButton").click(function() {
+    $.get("https://natba.github.io/AjaxExample/demo_test.asp", 
+          function(data, status){
+            $("#section2").html(data);
+	  });
+    });
+
+    $("#postButton").click(function(){
+      console.log("button2 clicked");
+      $.post("ajaxPost.php", 
+             {name: "Donald Duck",
+              city: "Duckburg"}, 
+	     function(data, status){
+               alert("Data: " + data + "\nStatus: " + status);
+             });
+    });
+});
--- AjaxSample/ajaxExample.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#section1").load("demo_test.txt");
-    });
-});
