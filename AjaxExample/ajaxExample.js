0acdfca569c74cc5b36c7484d9cf5149421c4714
natba <natba@hotmail.com>
2018-11-26 14:22:33 -0800

    Fix JS Ajax example

--- AjaxExample/ajaxExample.js
+++ AjaxExample/ajaxExample.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
     $("#postButton").click(function(){
       console.log("button2 clicked");
-      $.post("demo_test_post.php", 
+      $.post("demo_test_post.asp", 
              {name: "Donald Duck",
               city: "Duckburg"}, 
 	     function(data, status){
