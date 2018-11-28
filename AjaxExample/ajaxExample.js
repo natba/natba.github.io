b25c03cf66207f2b6098575f25226bf98d4f0af1
natba <natba@hotmail.com>
2018-11-28 11:47:04 -0800

    Code cleanup and comments

--- AjaxExample/ajaxExample.js
+++ AjaxExample/ajaxExample.js
@@ -1,50 +1,46 @@
 // CSD 122 Ajax Example - Nat Ballou
 $(document).ready(function(){
-  // Ajax load example
+  // Ajax load example - with logging
   $("#loadButton").click(function(){
-    $("#loadSection").load("demo_test.html", function(responseTxt, statusTxt, xhr){
-      if(statusTxt == "success")
-        alert("External content loaded successfully!");
-      else alert("Error: " + xhr.status + ": " + xhr.statusText);
-
-      console.log(responseTxt);
-      console.log(statusTxt);
-      console.log(xhr);
+    $("#loadSection").load("demo_test.html", 
+      function(responseTxt, statusTxt, xhr){
+        console.log(responseTxt);
+        console.log(statusTxt);
+        console.log(xhr);
     });
   });
 
-  // Ajax get example
+  // Ajax get example - with logging
   $("#getButton").click(function() {
     $.get("https://natba.github.io/AjaxExample/demo_test.js", 
           function(data, status){
             $("#getSection").html(data);
-	    console.log(data);
-	    console.log(status);
+            console.log(data);
+            console.log(status);
 	  });
-    });
+  });
 
   // Ajax post example - need server permission
   $("#postButton").click(function(){
     $.post("demo_test_post.php", 
-            {
-              name: "Donald Duck",
-              city: "Duckburg"
-            }, 
+           { name: "Donald Duck", city: "Duckburg" }, 
            function(data, status){
-             alert("Data: " + data + "\nStatus: " + status);
              $("#postSection").html(data);
+             console.log(data);
+             console.log(status);
            });
-    });
+  });
 
-  // Ajax put example
+  // Ajax put example - need server permission
   $("#putButton").click(function(){
     $.ajax({ url: 'demo_test.js',
              type: 'PUT',
              data: { name: "Micky Mouse", city: "Disney" },
              contentType: "application/json",
              success: function(data, status) {
-                        alert("Data: " + data + "\nStatus: " + status);
                         $("#putSection").html(data);
+                        console.log(data);
+                        console.log(status);
                       }
           });
     });
